dbf91b51c17332a55e7416f163c16bb3f1d17baa
RichVagos <87157952+RichVagos@users.noreply.github.com>
2021-07-16 17:14:34 -0500

    Update init.js

--- JS/init.js
+++ JS/init.js
@@ -2,11 +2,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
-    apiKey: "AIzaSyD6dFi4iiGUb0nucqvWGtGiFaBokCrGzV0",
+   apiKey: "AIzaSyD6dFi4iiGUb0nucqvWGtGiFaBokCrGzV0",
     authDomain: "auten4-22efd.firebaseapp.com",
     projectId: "auten4-22efd",
     storageBucket: "auten4-22efd.appspot.com",
     messagingSenderId: "941047250813",
     appId: "1:941047250813:web:b3d65c084a97a4057a5614",
     measurementId: "G-R0D6X76B7D"
+ 
 });
