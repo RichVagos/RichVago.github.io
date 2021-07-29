7fdceb1d10fa62bf47ce70ad371019463873b62c
RichVagos <87157952+RichVagos@users.noreply.github.com>
2021-07-28 19:34:18 -0500

    Update init.js

--- JS/init.js
+++ JS/init.js
@@ -2,12 +2,12 @@
  * configuración en tu servidor de Firebase.  */
 // @ts-ignore
 firebase.initializeApp({
- apiKey: "AIzaSyCG-6VLyWp67Lbe5tIZmcSzmchWxLRFhj8",
-    authDomain: "chat41-f5f5e.firebaseapp.com",
-    projectId: "chat41-f5f5e",
-    storageBucket: "chat41-f5f5e.appspot.com",
-    messagingSenderId: "643177349834",
-    appId: "1:643177349834:web:fddc07fde655298f33ea33",
-    measurementId: "G-2JY5ZWJYKQ"
- 
+
+  apiKey: "AIzaSyBGwT5Vaf2GCBRvHMrPu5Bq8AI_REM2pgM",
+    authDomain: "autenrichv.firebaseapp.com",
+    projectId: "autenrichv",
+    storageBucket: "autenrichv.appspot.com",
+    messagingSenderId: "357072619068",
+    appId: "1:357072619068:web:18ab87e1e64228cf8e6539",
+    measurementId: "G-82PLSHPFTS"
 });
